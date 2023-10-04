e524dd670ef033c73b1988128924e0584b5f5995
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-04 14:55:30 +0100

    Add files via upload

---
+++ ft_isdigit.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 09:42:04 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/07/31 11:23:51 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (2048);
+	else	
+		return (0);
+}
+/*
+#include <stdio.h>
+#include <ctype.h>
+int	main(void)
+{
+	int     a = 'a';
+        int     b = '3';
+        int     c = '5';
+	printf("%d\n", ft_isdigit(a));
+	printf("%d\n", ft_isdigit(b));
+	printf("%d\n", ft_isdigit(c));
+	printf("%d\n", isdigit(a));
+        printf("%d\n", isdigit(b));
+        printf("%d\n", isdigit(c));
+}*/
