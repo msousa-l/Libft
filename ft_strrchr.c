772b5f449fcdb56de4ba69b3f1e4c2139b12a954
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-06 13:11:54 +0100

    Add files via upload

---
+++ ft_strrchr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 15:28:05 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/10/06 13:11:39 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "libft.h"
+//#include <string.h>
+
+
+char	*ft_strrchr(const char *str, int c)
+{
+	int	i;
+
+	i = ft_strlen(str);
+	if ( c == 0)
+		return ((char *)str + i);
+	while (i >= 0)
+	{
+		if (str[i] == c)
+			return ((char *)str + i);
+		i--;
+	}
+	return (0);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+     	char	second[25] = "It was all a dream bwaby!";
+	//printf("%s\n", ft_strrchr(second, 'w'));
+	printf("%s\n", strrchr(second, 'w'));
+}*/
