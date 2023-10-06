842fe70bbc3aa72feb21862df9f192b75c6b41e1
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-06 11:49:59 +0100

    Add files via upload

---
+++ ft_strlcat.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 16:13:54 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/10/06 11:49:37 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#define "libf.h"
+#include <bsd/string.h>
+
+size_t	ft_strlcat(char *dst,const char *src, size_t size)
+{
+	size_t	i;
+	size_t	z;
+	size_t result;
+
+	i = 0;
+	z = 0;
+	result = 0;
+	while (src[result] != '\0')
+		result++;
+	while (dst[i] != '\0')
+		i++;
+	if (size <= i)
+		result += size;
+	else
+		result += i;
+	while (src[z] != '\0' && (i + 1) < size)
+	{
+		dst[i] = src[z];
+		z++;
+		i++;
+	}
+	dst[i] = '\0';
+	return (result);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	dst[20] = "Muito ";
+	char	src[8] = "bom dia";
+
+        printf("%zu\n",ft_strlcat(dst, src, 10));
+	printf("%zu\n", strlcat(dst, src, 10));
+	printf("%s\n", dst);
+}*/
