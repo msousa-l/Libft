7e7271757fdbc82326d01a24bc5a0c836eaf4991
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-06 21:55:30 +0100

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 15:28:05 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/10/06 13:11:39 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "libft.h"
+#include <string.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle,size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (needle == NULL )
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] == needle[j] && (i + j < len))
+			{
+				if (needle[j + 1] == '\0')
+					return ((char *)haystack + i);
+				j++;
+			}
+			j = 0;
+		}
+		i++;
+	}
+	return (NULL);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	first[4] = "all";
+     	char	second[25] = "It was all a dream bwaby!";
+	printf("%s\n", ft_strnstr(second, first, 15));
+	//printf("%s\n", strnstr(second, first, 4));
+}*/
