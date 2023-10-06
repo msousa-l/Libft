f23d3b46c16dc707d64c1bee9cc8f56452e75ec2
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-06 13:22:55 +0100

    Add files via upload

---
+++ ft_strncmp.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 10:47:55 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/10/06 13:22:39 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#define "libft.h"
+//#include <string.h>
+
+int	ft_strncmp(const char *s1, const  char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && (i < (n - 1)))
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	first[10] = "abcdefghi";
+        char	second[6] = "abcef";
+
+	//printf("%d\n", ft_strncmp(first, second, 4));
+	printf("%d", strncmp(first, second, 4));
+}*/	
