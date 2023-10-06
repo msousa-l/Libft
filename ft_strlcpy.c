50a27838108237048c366e7485b1b20c7954f27b
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-06 11:38:03 +0100

    Add files via upload

---
+++ ft_strlcpy.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 10:27:23 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/10/06 11:37:40 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "libft.h"
+#include <bsd/string.h>
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0' && i + 1 < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < size)
+		dest[i] = '\0';
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	dest[30] = "";
+	char	src[25] = "See you later alligator!";
+	size_t	result;
+
+	result = ft_strlcpy(dest, src, 0);
+	printf("length of src is %zu.", result);
+	printf("String dest now is : %s", dest);
+
+	//result = strlcpy(dest, src, 0);
+        //printf("length of src is %zu.", result);
+        //printf("String dest now is : %s", dest);
+
+}*/		
