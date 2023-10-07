22cf1702dc008f45e653a9a9848ace9eae6994fa
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-07 10:24:29 +0100

    Add files via upload

---
+++ ft_memcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 09:14:25 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/07/31 11:21:05 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//#include "libft.h"
+#include <string.h>
+void	*ft_memcpy(void *dest,const void *src, size_t n)
+{
+	size_t	i;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	 return (dest);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char a[20] = "ahahahahaah";
+	char b[20] = "bhbhbhbhbhb";
+	size_t len = sizeof(a);
+	ft_memcpy(b, a, len);
+
+	printf("string b contents after ft_memcpy:\n%s", b);
+	return 0;
+}*/
