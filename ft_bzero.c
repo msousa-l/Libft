10165bbcec004abf02ce80774c2b9824ba7925d4
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-07 10:13:27 +0100

    Add files via upload

---
+++ ft_bzero.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 09:14:25 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/07/31 11:21:05 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//#include "libft.h"
+#include <string.h>
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char a[20] = "Just testing!!!";
+
+    size_t len = sizeof(a);
+    ft_bzero(a, len);
+
+    printf("string a contents after ft_bzero:\n");
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("%c ",a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}*/
