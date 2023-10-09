17b582b67fd4a2443470572a67559056d22b0ea4
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-09 10:43:02 +0100

    Add files via upload

---
+++ ft_memchr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 09:14:25 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/10/09 10:42:22 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//#include "libft.h"
+#include <string.h>
+void	*ft_memchr(const void *str, int c, size_t n)
+{
+	size_t	i;
+	unsigned char	*p;
+
+	p = (unsigned char *)str;
+	i = 0;
+	while (i < n)
+	{
+		if (p[i] == (unsigned char)c)
+			return((unsigned char*)p + i);
+		i++;
+	 }
+	 return (NULL);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	a[] = "Qual sera que vai ser encontrada?";
+	int	b = ' ';
+	char	*result;
+
+	result = memchr(a, b, strlen(a));
+	
+	//printf("%s\n", result);
+	printf("%s\n", result);
+}*/
---
+++ ft_memmove.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 09:14:25 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/10/09 10:29:18 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//#include "libft.h"
+//#include <string.h>
+
+void	*ft_memmove(void *dest,const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t	i;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	i = 0;
+	if (d < s)
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = n;
+			while (i > 0)
+			{
+				d[i - 1] = s[i - 1];
+				i--;
+			}
+	}
+	return (dest);
+}
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char a[20] = "ahahahahaah";
+	char b[20] = "bhbhbhbhbhb";
+	size_t len = sizeof(a);
+	ft_memmove(b, a, len);
+
+	printf("string b contents after ft_memmove:\n%s", b);
+	return (0);
+}*/
