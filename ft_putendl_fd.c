fa270b0fbf1bcec6046e836a2ec98c378a333391
msousa-l <146946642+msousa-l@users.noreply.github.com>
2023-10-11 10:43:42 +0100

    Add files via upload

---
+++ ft_putendl_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 14:01:44 by msousa-l          #+#    #+#             */
+/*   Updated: 2023/10/11 10:43:23 by msousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#define "libft.h"
+#include<unistd.h>
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!*s || fd < 0)
+		return (0);
+	else
+		ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
+/*int	main(void)
+{
+	char	*str;
+	str = "foda-se";
+	ft_putstr_fd(str, 1);
+}*/
